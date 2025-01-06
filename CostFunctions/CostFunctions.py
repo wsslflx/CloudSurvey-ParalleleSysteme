30d64988c04aa4edf57c20e17a62cdd5e71066fa
Deckenlampe <FelixWissel@outlook.com>
2025-01-06 15:12:46 +0100

    Beginn Cost Function

---
+++ CostFunctions/CostFunctions.py
@@ -0,0 +1,15 @@
+def cost_one_job(duration, instancePrice):
+    return duration * instancePrice
+
+#fetch Instance Price from Database for specific time
+def get_instancePrice(instance, time):
+    cost = 0
+    return cost
+
+# return minimal price and start hour for given duration per Instance
+def cost_startHour(instance, duration):
+    hour = 0
+    cost = 0
+    return cost, hour
+
+
